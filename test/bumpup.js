9b9dd0d3859d9b493ffd8a4b9da8c4c91814fe6f
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2020-03-25 16:07:57 +0530

    updating

--- test/bumpup.js
+++ test/bumpup.js
@@ -11,7 +11,7 @@ var BUMP_UP_THRESHOLD = 14 * 24 * 60 * 60 * 1000;
 
 var data = require('./vehicle.json');
 
-describe('POST /vehicles/:id (bumpup)', function () {
+describe.only('POST /vehicles/:id (bumpup)', function () {
   var client;
   var vehicle;
   var image;
@@ -92,11 +92,11 @@ describe('POST /vehicles/:id (bumpup)', function () {
       if (e) {
         return done(e);
       }
-      r.statusCode.should.equal(errors.forbidden().status);
+      r.statusCode.should.equal(errors.notFound().status);
       should.exist(b);
       should.exist(b.code);
       should.exist(b.message);
-      b.code.should.equal(errors.forbidden().data.code);
+      b.code.should.equal(errors.notFound().data.code);
       done();
     });
   });
@@ -150,15 +150,109 @@ describe('POST /vehicles/:id (bumpup)', function () {
         if (e) {
           return done(e);
         }
-        r.statusCode.should.equal(200);
+        r.statusCode.should.equal(errors.notFound().status);
         should.exist(b);
-        should.exist(b.id);
-        should.exist(b.user);
-        b.id.should.equal(vehicle.id);
-        b.user.should.equal(vehicle.user);
-        var diff = new Date() - new Date(b.updatedAt);
-        diff.should.be.below(2000);
+        should.exist(b.code);
+        should.exist(b.message);
+        b.code.should.equal(errors.notFound().data.code);
         done();
+      });
+    });
+  });
+
+  describe('bump up on published vehicle', function () {
+
+    before(function (done) {
+      pot.publish('autos', 'vehicles', vehicle.id, client.users[0].token, client.admin.token, done);
+    });
+
+    it('bump up by an authorized user (too early)', function (done) {
+      Vehicles.update({_id: vehicle.id}, {updatedAt: Date.now()}, function (err) {
+        if (err) {
+          return done(err);
+        }
+        request({
+          uri: pot.resolve('autos', '/apis/v/vehicles/' + vehicle.id),
+          method: 'POST',
+          auth: {
+            bearer: client.users[0].token
+          },
+          headers: {
+            'X-Action': 'bumpup'
+          },
+          json: true
+        }, function (e, r, b) {
+          if (e) {
+            return done(e);
+          }
+          r.statusCode.should.equal(errors.forbidden().status);
+          should.exist(b);
+          should.exist(b.code);
+          should.exist(b.message);
+          b.code.should.equal(errors.forbidden().data.code);
+          done();
+        });
+      });
+    });
+
+    it('bump up by an unauthorized user', function (done) {
+      Vehicles.update({_id: vehicle.id}, {updatedAt: Date.now() - BUMP_UP_THRESHOLD}, function (err) {
+        if (err) {
+          return done(err);
+        }
+        request({
+          uri: pot.resolve('autos', '/apis/v/vehicles/' + vehicle.id),
+          method: 'POST',
+          auth: {
+            bearer: client.users[1].token
+          },
+          headers: {
+            'X-Action': 'bumpup'
+          },
+          json: true
+        }, function (e, r, b) {
+          if (e) {
+            return done(e);
+          }
+          r.statusCode.should.equal(errors.notFound().status);
+          should.exist(b);
+          should.exist(b.code);
+          should.exist(b.message);
+          b.code.should.equal(errors.notFound().data.code);
+          done();
+        });
+      });
+    });
+
+    it('by an authorized user', function (done) {
+      Vehicles.update({_id: vehicle.id}, {updatedAt: Date.now() - BUMP_UP_THRESHOLD}, function (err) {
+        if (err) {
+          return done(err);
+        }
+        request({
+          uri: pot.resolve('autos', '/apis/v/vehicles/' + vehicle.id),
+          method: 'POST',
+          auth: {
+            bearer: client.users[0].token
+          },
+          headers: {
+            'X-Action': 'bumpup'
+          },
+          json: true
+        }, function (e, r, b) {
+          if (e) {
+            return done(e);
+          }
+          r.statusCode.should.equal(200);
+          should.exist(b);
+          should.exist(b.id);
+          should.exist(b.user);
+          b.id.should.equal(vehicle.id);
+          b.user.should.equal(vehicle.user);
+          var diff = new Date() - new Date(b.updatedAt);
+          diff.should.be.below(5000);
+          done();
+        });
       });
     });
   });
